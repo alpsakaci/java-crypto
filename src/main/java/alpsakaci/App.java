141f9d550cfd5e0717b200d3d5cb2f982ea7a112
alpsakaci <erenalpsakaci@gmail.com>
2020-06-30 12:06:16 +0300

    app init

---
+++ src/main/java/alpsakaci/App.java
@@ -0,0 +1,13 @@
+package alpsakaci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
