2202bf94fafe3b2d1a3522c80d2fa52e812f2aed
Alp <erenalpsakaci@gmail.com>
2020-09-23 18:35:02 +0300

    pbkdf key derivation added

--- src/main/java/com/alpsakaci/crypto/App.java
+++ src/main/java/com/alpsakaci/crypto/App.java
@@ -1,13 +1,14 @@
 package com.alpsakaci.crypto;
 
 import java.security.KeyPair;
+import java.util.Base64;
 
 import at.favre.lib.crypto.bcrypt.BCrypt.HashData;
 
 public class App {
-	
+
 	public static void main(String[] args) {
-		// HASHING
+		System.out.println("----- HASHING -----");
 		String salt = Hashing.generateSalt();
 		System.out.println(salt);
 
@@ -17,20 +18,25 @@ public class App {
 		boolean t = Hashing.verifyHash("secret!", salt, hash.rawHash);
 		System.out.println(t);
 
-		// AES
+		System.out.println("----- AES -----");
 		String enc = AES.encrypt("test", "p@ssword".getBytes());
 		System.out.println(enc);
 
 		String dec = AES.decrypt(enc, "p@ssword".getBytes());
 		System.out.println(dec);
 
-		// RSA
+		System.out.println("----- RSA -----");
 		KeyPair keyPair = RSA.generateKey();
 		String enc1 = RSA.encrypt("plain", keyPair.getPublic());
 		System.out.println(enc1);
 		String dec1 = RSA.decrypt(enc1, keyPair.getPrivate());
 		System.out.println(dec1);
 
+		System.out.println("----- KEY DERIVATION -----");
+		String salt2 = Hashing.generateSalt();
+		byte[] generatedKey = KeyDerivation.generateKey("your_password", salt2, 100000, 512);
+		Base64.Encoder encoder = Base64.getEncoder();
+		System.out.println(encoder.encodeToString(generatedKey));
 	}
-	
+
 }
---
+++ src/main/java/com/alpsakaci/crypto/KeyDerivation.java
@@ -0,0 +1,23 @@
+package com.alpsakaci.crypto;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class KeyDerivation {
+	
+	public static byte[] generateKey(String password, String salt, int iterations, int keyLength) {
+		
+		try {
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), iterations, keyLength);
+			SecretKey secretKey = skf.generateSecret(keySpec);
+			
+			return secretKey.getEncoded();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+}
